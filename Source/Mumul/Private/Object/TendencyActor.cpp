5c04717da57c706c431965216d49f3dd2743009a
khc534854 <khc534854@gmail.com>
2025-12-22 23:31:12 +0900

    2025-12-22 대화형 npc

---
+++ Source/Mumul/Private/Object/TendencyActor.cpp
@@ -0,0 +1,112 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Object/TendencyActor.h"
+
+#include "Components/CapsuleComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Player/CuteAlienController.h"
+
+
+// Sets default values
+ATendencyActor::ATendencyActor()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	InteractionUI = CreateDefaultSubobject<UWidgetComponent>(TEXT("InteractionUI"));
+	InteractionUI->SetupAttachment(RootComponent);
+	InteractionUI->SetWidgetSpace(EWidgetSpace::Screen);
+	InteractionUI->SetVisibility(false);
+
+	InteractionUI->SetIsReplicated(false); 
+    
+	InteractionUI->SetVisibility(false);
+}
+
+// Called when the game starts or when spawned
+void ATendencyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (GetCapsuleComponent())
+	{
+		GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &ATendencyActor::OnOverlapBegin);
+		GetCapsuleComponent()->OnComponentEndOverlap.AddDynamic(this, &ATendencyActor::OnOverlapEnd);
+	}
+
+	if (InteractionUI)
+	{
+		InteractionUI->SetVisibility(false); // 기본 숨김
+	}
+	
+	
+	if (IdleMontage)
+	{
+		GetMesh()->SetAnimationMode(EAnimationMode::AnimationSingleNode);
+		GetMesh()->AnimationData.bSavedLooping = true;
+		GetMesh()->AnimationData.bSavedPlaying = true;
+		GetMesh()->AnimationData.AnimToPlay = IdleMontage;
+	}
+	UpdateBodyMaterial(TendencyIndex);
+}
+
+// Called every frame
+void ATendencyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ATendencyActor::UpdateBodyMaterial(int32 TendencyIdx)
+{
+	if (PlayerBodyMaterials.Num() == 0) return;
+
+	int32 MatIndexStart = 0;
+
+	if (TendencyIdx <= 1)
+	{
+		MatIndexStart = 0;
+	}
+	else if (TendencyIdx >= 2 && TendencyIdx <= 5)
+	{
+		MatIndexStart = (TendencyIdx - 1) * 3;
+	}
+
+	if (PlayerBodyMaterials.IsValidIndex(MatIndexStart + 2))
+	{
+		GetMesh()->SetMaterial(0, PlayerBodyMaterials[MatIndexStart]);
+		GetMesh()->SetMaterial(2, PlayerBodyMaterials[MatIndexStart + 1]);
+		GetMesh()->SetMaterial(3, PlayerBodyMaterials[MatIndexStart + 2]);
+	}
+}
+
+// Called to bind functionality to input
+void ATendencyActor::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ATendencyActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (APawn* Pawn = Cast<APawn>(OtherActor))
+	{
+		if (Pawn->IsLocallyControlled())
+		{
+			if (InteractionUI) InteractionUI->SetVisibility(true);
+		}
+	}	
+}
+
+void ATendencyActor::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (APawn* Pawn = Cast<APawn>(OtherActor))
+	{
+		if (Pawn->IsLocallyControlled())
+		{
+			if (InteractionUI) InteractionUI->SetVisibility(false);
+		}
+	}
+}
+
---
+++ Source/Mumul/Public/Object/TendencyActor.h
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TendencyActor.generated.h"
+
+UCLASS()
+class MUMUL_API ATendencyActor : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATendencyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tendency")
+	int32 TendencyIndex = 0;
+	
+	void UpdateBodyMaterial(int32 TendencyIdx);
+	
+	UPROPERTY(EditDefaultsOnly)
+	TArray<UMaterialInterface*> PlayerBodyMaterials;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UAnimMontage> IdleMontage;
+	
+	
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Dialogue")
+	TObjectPtr<class UWidgetComponent> InteractionUI;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+	FString DialogueText;
+	
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+};
